5a161a83ce149ffa106f4f5dffea6e3b8d54f2c6
Christian Michael <cmich004@ucr.edu>
2019-05-03 17:54:31 -0700

    WUS based growth update for apical view.

--- SBATCH_SCRIPT.sh
+++ SBATCH_SCRIPT.sh
@@ -4,12 +4,12 @@
 #SBATCH --cpus-per-task=48
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=0-36:00:00
-#SBATCH --output=my10.stdout
-#SBATCH --job-name="test_10"
+#SBATCH --output=my21.stdout
+#SBATCH --job-name="test_21"
 #SBATCH -p batch
 
 export OMP_NUM_THREADS 48
-mkdir Animate_test_10
-mkdir Nematic_test_1      
-mkdir Locations_test_1         
-./program Animate_test_10 Locations_test_1 Nematic_test_1                                  
+mkdir Animate_test_21
+mkdir Nematic_test_1 
+mkdir Locations_test_1 
+./program Animate_test_21 Locations_test_1 Nematic_test_1                                  
